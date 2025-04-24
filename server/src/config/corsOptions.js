53902cd635b3571f77471d1383d36f05c43c6da1
Halil Furkan <hfurkankarademir@gmail.com>
2025-04-24 22:26:27 +0300

    Update corsOptions.js

--- server/src/config/corsOptions.js
+++ server/src/config/corsOptions.js
@@ -1,5 +1,5 @@
 export const corsConfig = {
-    origin: "http://localhost:5173" || "https://sosyapp.vercel.app/",
+    origin: "http://localhost:5173" || "https://sosyapp.vercel.app",
     credentials: true,
     exposedHeaders: ["Set-Cookie"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
